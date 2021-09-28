486430187bc8d5f3e7d8e5c72695437280831d87
SamuelDear <samuel_dear@hotmail.com>
2021-09-27 21:32:57 -0400

    Fix placeholder color

--- src/styles/components/TodoSearch.ts
+++ src/styles/components/TodoSearch.ts
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core';
 const useStyles = makeStyles(() => ({
   input: {
     color: '#FFF',
+    '&::': {
+      color: '#FFF',
+    },
   },
   colorIcon: {
     color: '#FFF',
