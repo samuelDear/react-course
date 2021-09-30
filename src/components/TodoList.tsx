4166b46443f39bfcae1a66ac59ac755983c793ba
SamuelDear <samuel_dear@hotmail.com>
2021-09-29 23:01:45 -0400

    Add dnd for mobile

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Grid, Box } from '@material-ui/core';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
+import { isMobile } from 'react-device-detect';
 import { useStyleTodoList } from 'styles';
 
 const TodoList: React.FC = ({ children }) => {
@@ -10,7 +12,7 @@ const TodoList: React.FC = ({ children }) => {
   return (
     <Box my={4} display="flex" className={classes.boxList}>
       <Grid container item xs={12}>
-        <DndProvider backend={HTML5Backend}>{children}</DndProvider>
+        <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>{children}</DndProvider>
       </Grid>
     </Box>
   );
